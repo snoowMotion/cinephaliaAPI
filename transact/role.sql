b92b7c6d61925c095d8291f6c663def8433898f9
Guillaume C <carpentier.guillaume.62@gmail.com>
2024-12-18 16:49:22 +0100

    [refacto] Correction nom de rôles

--- transact/role.sql
+++ transact/role.sql
@@ -2,12 +2,12 @@ BEGIN
 
 -- Insertion des données dans la table role
 
-INSERT INTO role(libelle) VALUES ('api-lecture');
-INSERT INTO role(libelle) VALUES ('api-ecriture');
-INSERT INTO role(libelle) VALUES ('api-admin');
-INSERT INTO  role(libelle) VALUES ('utilisateur');
-INSERT INTO  role(libelle) VALUES ('visiteur');
-INSERT INTO  role(libelle) VALUES ('employe');
+INSERT INTO role(libelle) VALUES ('ROLE_API_LECTURE');
+INSERT INTO role(libelle) VALUES ('ROLE_API_ECRITURE');
+INSERT INTO role(libelle) VALUES ('ROLE_API_ADMIN');
+INSERT INTO  role(libelle) VALUES ('ROLE_USER');
+INSERT INTO  role(libelle) VALUES ('ROLE_GUEST');
+INSERT INTO  role(libelle) VALUES ('ROLE_EMPLOYE');
 
 COMMIT;
 
