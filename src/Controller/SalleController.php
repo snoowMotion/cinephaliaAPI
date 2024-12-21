7f5dd375798e5d791b70524d8f3e754bf94ec5a3
Guillaume C <carpentier.guillaume.62@gmail.com>
2024-12-21 11:07:11 +0100

    francisation des retours

--- src/Controller/SalleController.php
+++ src/Controller/SalleController.php
@@ -31,12 +31,17 @@ class SalleController
         // On vérifie que le cinema existe
         $cinema = $entityManager->getRepository(Cinema::class)->find($data['cinema']);
         if (empty($cinema)) {
-            return new JsonResponse(['status' => 'Cinema not found'], Response::HTTP_NOT_FOUND);
+            return new JsonResponse(['status' => 'Cinema introuvable'], Response::HTTP_NOT_FOUND);
         }
         // On vérifie que la qualité existe
         $qualite = $entityManager->getRepository(Qualite::class)->find($data['qualite']);
         if (empty($qualite)) {
-            return new JsonResponse(['status' => 'Qualite not found'], Response::HTTP_NOT_FOUND);
+            return new JsonResponse(['status' => 'Qualite introuvable'], Response::HTTP_NOT_FOUND);
+        }
+        // On vérifie que le numéro de salle n'est pas déjà utilisé
+        $salle = $entityManager->getRepository(Salle::class)->findOneBy(['numSalle' => $data['numSalle']]);
+        if (!empty($salle)) {
+            return new JsonResponse(['status' => 'Numéro de salle déjà utilisé'], Response::HTTP_CONFLICT);
         }
         // On crée la salle
         $salle = new Salle();
@@ -64,6 +69,6 @@ class SalleController
         $entityManager->persist($salle);
         $entityManager->flush();
 
-        return new JsonResponse(['status' => 'Salle created!'], Response::HTTP_CREATED);
+        return new JsonResponse(['status' => 'Salle crée!'], Response::HTTP_CREATED);
     }
 }
