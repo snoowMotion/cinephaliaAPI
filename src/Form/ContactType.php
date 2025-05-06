6f56ccbf0d7365ac61f8a8897a7ee3939156aff7
Guillaume C <carpentier.guillaume.62@gmail.com>
2025-05-06 14:54:54 +0200

    Création du formulaire contact Type

---
+++ src/Form/ContactType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'required' => false,
+                'label' => "Nom d'utilisateur (facultatif)",
+            ])
+            ->add('subject', TextType::class, [
+                'label' => 'Titre de la demande',
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Description',
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
